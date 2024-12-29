104bcec72825ff802e83da89ff654c8e71b64aea
Mayur Singh Shekhawat <88105485+Mayursingh9111@users.noreply.github.com>
2024-12-30 04:09:39 +0530

    Update index.js

--- Protfolio/src/constants/index.js
+++ Protfolio/src/constants/index.js
@@ -3,7 +3,7 @@ import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.png";
 import project4 from "../assets/projects/project-4.png";
 
-export const HERO_CONTENT = `I am a passionate Front end developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React.js My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate Front end developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologie like React.js My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
 export const ABOUT_TEXT = `I am a dedicated and versatile front end developer with a passion for creating efficient and user-friendly web applications. I have worked with a variety of technologies, including React.js My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
